a26632c78160a09f751772fff8093f0d98efc163
desbma <desbma@users.noreply.github.com>
2014-12-05 20:14:23 +0100

    Don't duplicate version string

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import re
+
 from setuptools import find_packages, setup
 
-VERSION = "1.1.0"
 
+with open(os.path.join("sacad", "__init__.py"), "rt") as f:
+  version = re.search("__version__ = \"([^\"]+)\"", f.read()).group(1)
 
 with open("requirements.txt", "rt") as f:
   requirements = f.read().splitlines()
@@ -13,7 +17,7 @@ with open("README.md", "rt") as f:
   readme = f.read()
 
 setup(name="sacad",
-      version=VERSION,
+      version=version,
       author="desbma",
       packages=find_packages(),
       entry_points={"console_scripts": ["sacad = sacad:cl_main"]},
@@ -22,7 +26,7 @@ setup(name="sacad",
       description="Search and download music album covers",
       long_description=readme,
       url="https://github.com/desbma/sacad",
-      download_url="https://github.com/desbma/sacad/archive/%s.tar.gz" % (VERSION),
+      download_url="https://github.com/desbma/sacad/archive/%s.tar.gz" % (version),
       keywords=["download", "album", "cover", "art", "albumart", "music"],
       classifiers=["Development Status :: 5 - Production/Stable",
                    "Environment :: Console",
