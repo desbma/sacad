9b8d1c074eb85da3f82e8a040f81a116390f398e
desbma <desbma@users.noreply.github.com>
2018-03-25 23:43:22 +0200

    Version 2.1.0

--- sacad/__init__.py
+++ sacad/__init__.py
@@ -2,7 +2,7 @@
 
 """ Smart Automatic Cover Art Downloader : search and download music album covers. """
 
-__version__ = "2.0.6"
+__version__ = "2.1.0"
 __author__ = "desbma"
 __license__ = "MPL 2.0"
 
