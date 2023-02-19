4ad35ecede3f867025e3ebb3b2f9085f9ad6af89
desbma <desbma@users.noreply.github.com>
2023-02-19 14:32:04 +0100

    fix: update UA to fix Amazon sources 503 errors

--- sacad/sources/amazonbase.py
+++ sacad/sources/amazonbase.py
@@ -25,7 +25,7 @@ class AmazonBaseCoverSource(CoverSource):
     def updateHttpHeaders(self, headers):
         """See CoverSource.updateHttpHeaders."""
         # mimic Firefox headers
-        headers["User-Agent"] = self.ua
+        headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0"
         headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
         headers["Accept-Language"] = "en-US,en;q=0.9"
         headers["DNT"] = "1"
