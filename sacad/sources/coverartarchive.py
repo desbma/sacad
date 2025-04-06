5c7fe4cf9c527a7e4ff317e41a915ae5ee7cf787
Koizel <koizel.vtknz@aleeas.com>
2025-04-06 14:17:50 +0200

    Ruff formatting

--- sacad/sources/coverartarchive.py
+++ sacad/sources/coverartarchive.py
@@ -8,25 +8,34 @@ import json
 
 
 class CoverArtArchiveSource(CoverSource):
-    #BASE_URL = "https://coverartarchive.org"
+    """
+    Cover source using the Cover Art Archive official API.
+
+    The documentation of the API is available at
+    https://musicbrainz.org/doc/Cover_Art_Archive/API
+
+    """
+
+    # The cover art archive is tied with Musicbrainz.
+    # Therefore, in order to get a cover, one must first get a release
+    # identifier or release group identifier.
+    # To get the release (or release group) id, one can either read from
+    # the tags (which isn't allowed for now in sacad), or one can ask the
+    # musicbrainz database for identifiers from the album and artist name.
+    # See https://musicbrainz.org/doc/MusicBrainz_API for the API of
+    # musicbrainz.
     BASE_URL = "https://musicbrainz.org/ws/2"
     API_KEY = ""
     API_SECRET = ""
 
     def getSearchUrl(self, album, artist):
-        # For the cover art archive, one must first retrieve the
-        # Musicbrainz ID of at least the release group and then use that
-        # id to fetch the cover from the cover art archive
-        # See https://musicbrainz.org/doc/Cover_Art_Archive/API
-        # On how to retrieve the release group id, see
-        # https://musicbrainz.org/doc/MusicBrainz_API
         return CoverSource.assembleUrl(
             f"{self.BASE_URL}/release-group", {"query": f"artist:{artist} AND album:{album}", "fmt": "json"}
         )
 
     def updateHttpHeaders(self, headers):
         headers["User-Agent"] = f"sacad/{__version__}"
-        
+
     async def parseResults(self, api_data):
         release_group = json.loads(api_data)
         mbid = release_group.release_groups[0].id
@@ -37,11 +46,11 @@ class CoverArtArchiveSource(CoverSource):
                 for rank, x in enumerate(cover_json.images, 1):
                     yield CoverSourceResult(
                         urls=x.image,
-                        size = None,
-                        format = CoverImageFormat.NONE,
+                        size=None,
+                        format=CoverImageFormat.NONE,
                         rank=rank,
-                        thumbnail_url = x.thumbnails.small, 
+                        thumbnail_url=x.thumbnails.small,
                         source_quality=CoverSourceQuality.EXACT_SEARCH,
-                        metadata = CoverImageMetadata.NONE,
-                        check_metadata = 1
+                        metadata=CoverImageMetadata.NONE,
+                        check_metadata=1,
                     )
