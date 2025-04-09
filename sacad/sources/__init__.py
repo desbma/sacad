dd43f64335047c1270f0a0ba5c05c6b7d783b06f
Koizel <koizel.vtknz@aleeas.com>
2025-04-09 21:02:38 +0200

    Register the new source

--- sacad/sources/__init__.py
+++ sacad/sources/__init__.py
@@ -4,3 +4,5 @@ from sacad.sources.deezer import DeezerCoverSource, DeezerCoverSourceResult  # n
 from sacad.sources.discogs import DiscogsCoverSource, DiscogsCoverSourceResult  # noqa: F401
 from sacad.sources.itunes import ItunesCoverSource, ItunesCoverSourceResult  # noqa: F401
 from sacad.sources.lastfm import LastFmCoverSource, LastFmCoverSourceResult  # noqa: F401
+from sacad.sources.coverartarchive import CoverArtArchiveSource # noqa: F401
+
